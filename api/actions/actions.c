ef61e086cd0765e22dd6730cb70ae31bd8c301b3
Андрей Охота <sssmetannikov.a@gmail.com>
2024-07-30 04:38:20 +0700

    feat: physics.c

--- api/actions/actions.c
+++ api/actions/actions.c
@@ -7,30 +7,28 @@ void handle_actions() {
 }
 
 static void handle_input() {
-    handle_top_player();
-    handle_bottom_player();
+    if (_kbhit()) {
+        handle_top_player();
+        handle_bottom_player();
+    }
 }
 
 static void handle_top_player() {
-    if (_kbhit()) {
-        char ch = _getch();
-        if (ch == 'z') {
-            player_top.location_x -= 1;
-        }
-        if (ch == 'x') {
-            player_top.location_x += 1;
-        }
+    char ch = _getch();
+    if (ch == 'z') {
+        player_top.location_x -= 1;
+    }
+    if (ch == 'x') {
+        player_top.location_x += 1;
     }
 }
 
 static void handle_bottom_player() {
-    if (_kbhit()) {
-        char ch = _getch();
-        if (ch == 'n') {
-            player_bottom.location_x -= 1;
-        }
-        if (ch == 'm') {
-            player_bottom.location_x += 1;
-        }
+    char ch = _getch();
+    if (ch == 'n') {
+        player_bottom.location_x -= 1;
+    }
+    if (ch == 'm') {
+        player_bottom.location_x += 1;
     }
 }
