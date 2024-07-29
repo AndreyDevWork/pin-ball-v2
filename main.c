d1536b5a28ace8acf0e45fbfa9cda64657a71260
Андрей Охота <sssmetannikov.a@gmail.com>
2024-07-30 01:07:06 +0700

    feat: Sleep()

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
+#include <state.h>
+
 #include "game.h"
 #include "renderer.h"
+#include <windows.h>
 
 #include <stdio.h>
 
 
 int main(void) {
     init_state();
-    render();
+
+    while (1) {
+        render();
+        ball.ball_next_x++;
+        Sleep(100);
+    }
 
     return 0;
 }
